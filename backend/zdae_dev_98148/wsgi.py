dccf34344c7a3f98f2065f72bc7e3eb47ab020b0
Crowdbotics <engineering@crowdbotics.com>
2023-07-06 06:43:12 +0000

    Initial commit.

---
+++ backend/zdae_dev_98148/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for zdae_dev_98148 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zdae_dev_98148.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "zdae-dev-98148",
+  url: "https://zdae-dev-98148.botics.co",
+  api: "https://zdae-dev-98148.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
